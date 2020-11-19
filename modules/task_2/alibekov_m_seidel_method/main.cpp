dccbbc1fbfd492e184d2c1bd50921bd12020b90e
AlibekovMurad5202 <MuradAlibekov2000@gmail.com>
2020-11-19 16:56:33 +0300

    test_01

--- modules/task_2/alibekov_m_seidel_method/main.cpp
+++ modules/task_2/alibekov_m_seidel_method/main.cpp
@@ -5,7 +5,7 @@
 #include "iostream"
 #include "./seidel_method.h"
 
-TEST(Seidel_Method, my_SLAE_3_variables_parallel) {
+TEST(Seidel_Method, my_SLAE_3_variables_sequential) {
     int proc_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &proc_rank);
 
@@ -20,11 +20,11 @@ TEST(Seidel_Method, my_SLAE_3_variables_parallel) {
     std::vector<double> b = { 3., 7., 5. };
     std::vector<double> x(size);
 
-    if (proc_rank == 0) start_time = MPI_Wtime();
-    x = solving_SLAE_parallel(A, b);
-    if (proc_rank == 0) end_time = MPI_Wtime();
-
     if (proc_rank == 0) {
+        start_time = MPI_Wtime();
+        x = solving_SLAE_sequential(A, b);
+        end_time = MPI_Wtime();
+
         std::cout << "A: ";
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++)
@@ -56,7 +56,7 @@ TEST(Seidel_Method, my_SLAE_3_variables_parallel) {
     }
 }
 
-TEST(Seidel_Method, my_SLAE_3_variables_sequential) {
+TEST(Seidel_Method, my_SLAE_3_variables_parallel) {
     int proc_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &proc_rank);
 
@@ -71,11 +71,11 @@ TEST(Seidel_Method, my_SLAE_3_variables_sequential) {
     std::vector<double> b = { 3., 7., 5. };
     std::vector<double> x(size);
 
-    if (proc_rank == 0) {
-        start_time = MPI_Wtime();
-        x = solving_SLAE_sequential(A, b);
-        end_time = MPI_Wtime();
+    if (proc_rank == 0) start_time = MPI_Wtime();
+    x = solving_SLAE_parallel(A, b);
+    if (proc_rank == 0) end_time = MPI_Wtime();
 
+    if (proc_rank == 0) {
         std::cout << "A: ";
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++)
